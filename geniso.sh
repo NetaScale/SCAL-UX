217498b36e27f9fbfb4cf18591e7e75df8b09701
NetBSD User <netbsduser@users.noreply.github.com>
2022-05-31 18:21:17 +0100

    vxkern: Mesonic limine barebones

---
+++ geniso.sh
@@ -0,0 +1,15 @@
+rm vendor/limine/limine-deploy
+make -C vendor/limine
+
+	rm -rf iso_root
+	mkdir -p iso_root
+	cp build/vxkern/vxkern \
+		limine.cfg vendor/limine/limine.sys vendor/limine/limine-cd.bin \
+		vendor/limine/limine-cd-efi.bin iso_root/
+	xorriso -as mkisofs -b limine-cd.bin \
+		-no-emul-boot -boot-load-size 4 -boot-info-table \
+		--efi-boot limine-cd-efi.bin \
+		-efi-boot-part --efi-boot-image --protective-msdos-label \
+		iso_root -o barebones.iso
+	vendor/limine/limine-deploy barebones.iso
+	rm -rf iso_root
